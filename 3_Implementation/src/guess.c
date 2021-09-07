69711e29e1af050bd2c7fb0bbc1c2cf45608e974
Bamini-Varalaxmi <89648206+Bamini-Varalaxmi@users.noreply.github.com>
2021-09-07 19:39:09 +0530

    Update guess.c

--- 3_Implementation/src/guess.c
+++ 3_Implementation/src/guess.c
@@ -1,1 +1,19 @@
+#include "game.h"
+#include<stdio.h>
+int guess(int guesses){
+    int num=5, flag, count=0;
+    if(num==guesses) {
+         flag=0;
+         printf("Congratulations! You guessed the correct number %d\n", num);
+      } else if(guesses<num) {
+         flag=1;
+         printf("Your guess is lower than the number\n");
+         count++;
+      } else {
+         flag=1;
+         printf("Your guess is greater than the number\n");
+         count++;
+      }
+    return (count);
+}
 
