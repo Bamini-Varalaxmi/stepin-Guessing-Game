d0ade12dc25bd1b13582abd668a31cfd27a6f07e
Bamini-Varalaxmi <89648206+Bamini-Varalaxmi@users.noreply.github.com>
2021-09-07 19:41:52 +0530

    Create game.h

---
+++ 3_Implementation/inc/game.h
@@ -0,0 +1,15 @@
+/**
+ * @file game.h
+ * @author varalaxmi
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __GAME_H
+#define __GAME_H
+int guess();
+int game();
+#endif
