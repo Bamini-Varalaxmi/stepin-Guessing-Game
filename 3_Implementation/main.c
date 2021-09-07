1e7fc36fc265dc57003f58d5b2ac30175881afb2
Bamini-Varalaxmi <89648206+Bamini-Varalaxmi@users.noreply.github.com>
2021-09-07 19:21:17 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,1 +1,34 @@
-
+/**
+ * @file main.c
+ * @author varalaxmi
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "game.h"
+#include<stdio.h>
+int main() {
+   int num=5, flag=1, guesses, count=0, random1, random2;
+   printf("guess the number randomly within 1 to 10 range\n");
+   do {
+      printf("enter number");
+      scanf("%d", &guesses);
+      count++;
+      if(guesses>10 || guesses <1){
+         printf("please select the number within the range");
+         continue;
+      }
+      guess(guesses);
+      if(num==guesses){
+         flag=0;
+      printf("guessed properly ");
+      }
+      else{
+      random2= game();
+      }
+   } while(flag);
+   printf("Total number of trails you attempted for guessing is: %d\n",count);
+}
