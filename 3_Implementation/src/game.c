ac146857e474919f82918d2b536325b48a7f7f48
Bamini-Varalaxmi <89648206+Bamini-Varalaxmi@users.noreply.github.com>
2021-09-08 20:15:00 +0530

    Update game.c

--- 3_Implementation/src/game.c
+++ 3_Implementation/src/game.c
@@ -1,3 +1,13 @@
+/**
+ * @file main.c
+ * @author varalaxmi
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include "game.h"
 #include<stdio.h>
 int game(){
